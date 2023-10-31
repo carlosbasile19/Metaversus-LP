ff1d1e9e60e127c884fa9f3e73b5c5462f0ad867
Carlitos <basilecarlos12@gmail.com>
2023-10-31 08:13:00 +0100

    Progress done

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_CSS_MANIFEST={"__entry_css__":{"/Users/carlos/Documents/Programming/landing_page/app/page":["/Users/carlos/Documents/Programming/landing_page/styles/globals.css"]},"/Users/carlos/Documents/Programming/landing_page/app/layout.js":["/Users/carlos/Documents/Programming/landing_page/styles/globals.css"]}
